7e64988a86b98a4d941ca8f9905401900de679d6
maialenz <maialenzumeta@gmail.com>
2021-08-12 15:03:04 +0000

    updated mysql-from-python.py file

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,10 +14,11 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with the same number of placeholders as in list_of_names
+        format_strings = ', '.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({})".format(format_strings), list_of_names)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was succesful
     connection.close()
